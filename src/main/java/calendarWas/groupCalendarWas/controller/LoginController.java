d2ecd195a46b9d74cbb111ced5947329a636f47d
shc6260 <shc6260@gmail.com>
2025-10-07 21:53:24 +0900

    에러코드 주석

--- src/main/java/calendarWas/groupCalendarWas/controller/LoginController.java
+++ src/main/java/calendarWas/groupCalendarWas/controller/LoginController.java
@@ -2,7 +2,6 @@ package calendarWas.groupCalendarWas.controller;
 
 import calendarWas.groupCalendarWas.dto.LoginDto;
 import calendarWas.groupCalendarWas.service.LoginService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -19,27 +18,27 @@ public class LoginController {
 
     private final LoginService loginService;
 
-    @PostMapping("/login")
-    public String login(@Valid LoginDto form) {
-
-        if (bindingResult.hasErrors()) {
-            return "login/loginForm";
-        }
-
-        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
-
-        if (loginMember == null) {
-            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
-            return "login/loginForm";
-        }
-
-        //로그인 성공 처리
-        //세션이 있으면 있는 세션 반환, 없으면 신규 세션을 생성
-        HttpSession session = request.getSession();
-        //세션에 로그인 회원 정보 보관
-        session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
-
-        return "redirect:" + redirectURL;
-
-    }
+//    @PostMapping("/login")
+//    public String login(@Valid LoginDto form) {
+//
+//        if (bindingResult.hasErrors()) {
+//            return "login/loginForm";
+//        }
+//
+//        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+//
+//        if (loginMember == null) {
+//            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
+//            return "login/loginForm";
+//        }
+//
+//        //로그인 성공 처리
+//        //세션이 있으면 있는 세션 반환, 없으면 신규 세션을 생성
+//        HttpSession session = request.getSession();
+//        //세션에 로그인 회원 정보 보관
+//        session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
+//
+//        return "redirect:" + redirectURL;
+//
+//    }
 }
